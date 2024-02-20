780780d08892570d436ce4c1ba940694902d0bcf
Haifa Gaza <haifalapuz@gmail.com>
2024-02-20 16:41:41 +0900

    Add processor

---
+++ artillery-tests/tests/processors/processor.js
@@ -0,0 +1,42 @@
+module.exports = {
+    setVertexEventId: setVertexEventId,
+    setEdgeEventId: setEdgeEventId,
+    setEdgeEventIdLoop: setEdgeEventIdLoop,
+    hasNextEvent: hasNextEvent,
+};
+
+let eventTime = 10000;
+
+let vertexEventId = 0;
+function setVertexEventId(context, events, done) {
+    context.vars["vertexEventId"] = vertexEventId + "_" + eventTime;
+    vertexEventId += 1;
+    eventTime += 1;
+    return done();
+}
+
+let inVertex = 0;
+let outVertex = 5;
+function setEdgeEventId(context, events, done){
+    context.vars["edgeEventId"] = outVertex + "|label|" + inVertex + "_" + eventTime;
+    inVertex += 1;
+    outVertex += 1;
+    eventTime += 1;
+    return done();
+}
+
+let maxNeighbors = 1;
+let currNeighborId = 1;
+function hasNextEvent(context, next) {
+    const continueLooping = currNeighborId < maxNeighbors;
+    // While `continueLooping` is true, the `next` function will
+    // continue the loop in the test scenario.
+    return next(continueLooping);
+}
+
+function setEdgeEventIdLoop(context, events, done){
+    context.vars["edgeEventId"] = vertexEventId + "|label|" + currNeighborId + "_" + eventTime;
+    currNeighborId += 1;
+    eventTime += 1;
+    return done();
+}
