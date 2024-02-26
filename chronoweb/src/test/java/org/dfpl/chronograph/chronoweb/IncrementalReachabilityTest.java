3eefc1934e7c7df94e31684a091c6ba100879751
Haifa Gaza <haifalapuz@gmail.com>
2024-02-26 12:39:48 +0900

    Load datasets for the elapsed time analysis

--- chronoweb/src/test/java/org/dfpl/chronograph/chronoweb/IncrementalReachabilityTest.java
+++ chronoweb/src/test/java/org/dfpl/chronograph/chronoweb/IncrementalReachabilityTest.java
@@ -1,79 +1,109 @@
 package org.dfpl.chronograph.chronoweb;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
 
 public class IncrementalReachabilityTest {
 
-	@SuppressWarnings("deprecation")
 	public static void main(String[] args) throws IOException {
 		// INPUT
-		Integer source = 0;
-		Integer neighbors = 1000;
-		String filePath = "D:\\chronoweb\\results\\incr_8_4.txt";
+		Integer sourceVertex = 9;
+		Integer sourceTime = 0;
+		String filePath = "D:\\chronoweb\\results\\incr_8_4.txt"; // results
+		String graphFile = "D:\\chronoweb\\datasets\\bitcoin-alpha.txt"; // input graph
 
-		OutputStreamWriter osw;
 		String host = "http://localhost/chronoweb";
-		Integer baseTime = 9999;
-
-		Map<String, String> payload = new HashMap<>();
-		payload.put("k", "v");
 
 		// 1. SUBSCRIBE
-		String subURLString = String.format("%s/graph/%s/IsAfterReachability/%s", host, baseTime - 1, source);
+		subscribe(host, sourceVertex, sourceTime);
+
+		int lineRead = 0;
+		BufferedReader br = new BufferedReader(new FileReader(graphFile));
+		while (true) {
+			String line = br.readLine();
+			if (line == null)
+				break;
+
+			String[] arr = line.split("\\s");
+
+			Integer from = Integer.parseInt(arr[0]);
+			Integer to = Integer.parseInt(arr[1]);
+
+			Integer time = Integer.parseInt(arr[2]);
+
+			// 2. POST
+			postEdgeEvent(host, from, to, time);
+
+			Long startTime = System.currentTimeMillis();
+			// 3. GET
+			getReachability(host, sourceVertex, sourceTime);
+			Long computationTime = System.currentTimeMillis() - startTime;
+
+			BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, true));
+			writer.append(computationTime.toString()).append("\n");
+			writer.close();
+
+			if (++lineRead % 10000 == 0) {
+				System.out.println(lineRead + " lines read...");
+			}
+		}
+
+		br.close();
+	}
+
+	@SuppressWarnings("deprecation")
+	private static void getReachability(String host, Integer sourceVertex, Integer sourceTime) throws IOException {
+		String getURLString = String.format("%s/gammaTable/%s/IsAfterReachability/%s", host, sourceTime, sourceVertex);
+
+		URL getURL = new URL(getURLString);
+
+		HttpURLConnection getCon = (HttpURLConnection) getURL.openConnection();
+		getCon.setRequestMethod("GET");
+		System.out.println("\tGET " + getURLString + " " + getCon.getResponseCode());
+
+	}
+
+	@SuppressWarnings("deprecation")
+	private static void postEdgeEvent(String host, Integer from, Integer to, Integer time) throws IOException {
+		String postURLString = String.format("%s/graph/%s|label|%s_%s", host, from, to, time);
+		URL postURL = new URL(postURLString);
+		HttpURLConnection postCon = (HttpURLConnection) postURL.openConnection();
+		postCon.setRequestMethod("PUT");
+		postCon.setDoOutput(true);
+		postCon.setRequestProperty("Content-Type", "application/json");
+		postCon.setRequestProperty("Accept", "application/json");
+
+		OutputStreamWriter osw = new OutputStreamWriter(postCon.getOutputStream());
+		osw.write("{ \"k\": \"v\"}");
+		osw.flush();
+		osw.close();
+		System.out.println("POST " + postURLString + " " + postCon.getResponseCode());
+
+		postCon.disconnect();
+	}
+
+	@SuppressWarnings("deprecation")
+	public static void subscribe(String host, Integer sourceVertex, Integer sourceTime) throws IOException {
+		String subURLString = String.format("%s/graph/%s/IsAfterReachability/%s", host, sourceTime, sourceVertex);
 		URL subURL = new URL(subURLString);
 		HttpURLConnection subCon = (HttpURLConnection) subURL.openConnection();
 		subCon.setRequestMethod("PUT");
 		subCon.setDoOutput(true);
 		subCon.setRequestProperty("Content-Type", "application/json");
 		subCon.setRequestProperty("Accept", "application/json");
-		osw = new OutputStreamWriter(subCon.getOutputStream());
+
+		OutputStreamWriter osw = new OutputStreamWriter(subCon.getOutputStream());
 		osw.write("{ \"k\": \"v\"}");
 		osw.flush();
 		osw.close();
+
 		System.out.println("SUBSCRIBE " + subURLString + " " + subCon.getResponseCode());
 		subCon.disconnect();
-
-		for (int destination = 1; destination < neighbors + 1; destination++) {
-			Integer eventTime = baseTime + 1;
-
-			// 2. POST
-			String postURLString = String.format("%s/graph/%s|label|%s_%s", host, source, 1, eventTime);
-			URL postURL = new URL(postURLString);
-			HttpURLConnection postCon = (HttpURLConnection) postURL.openConnection();
-			postCon.setRequestMethod("PUT");
-			postCon.setDoOutput(true);
-			postCon.setRequestProperty("Content-Type", "application/json");
-			postCon.setRequestProperty("Accept", "application/json");
-			osw = new OutputStreamWriter(postCon.getOutputStream());
-			osw.write("{ \"k\": \"v\"}");
-			osw.flush();
-			osw.close();
-			System.out.println("POST " + postURLString + " " + postCon.getResponseCode());
-			postCon.disconnect();
-
-			// 3. GET
-			Long startTime = System.currentTimeMillis();
-			String getURLString = String.format("%s/gammaTable/%s/IsAfterReachability/%s", host, baseTime - 1, source);
-
-			URL getURL = new URL(getURLString);
-
-			HttpURLConnection getCon = (HttpURLConnection) getURL.openConnection();
-			getCon.setRequestMethod("GET");
-			System.out.println("\tGET " + getURLString + " " + getCon.getResponseCode());
-
-			Long computationTime = System.currentTimeMillis() - startTime;
-
-			BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, true));
-			writer.append(computationTime.toString()).append("\n");
-			writer.close();
-		}
 	}
-
 }
--- chronoweb/src/test/java/org/dfpl/chronograph/chronoweb/NaiveReachabilityTest.java
+++ chronoweb/src/test/java/org/dfpl/chronograph/chronoweb/NaiveReachabilityTest.java
@@ -2,6 +2,7 @@ package org.dfpl.chronograph.chronoweb;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,88 +10,110 @@ import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 
 public class NaiveReachabilityTest {
 
-	@SuppressWarnings("deprecation")
 	public static void main(String[] args) throws IOException {
 		// INPUT
-		Integer source = 0;
-		Integer neighbors = 1000;
-		String filePath = "D:\\chronoweb\\results\\8_4.txt";
+		Integer sourceVertex = 9;
+		Integer sourceTime = 0;
+		String filePath = "D:\\chronoweb\\results\\naive-bitcoin-alpha.txt"; // results
+		String graphFile = "D:\\chronoweb\\datasets\\bitcoin-alpha.txt"; // input graph
 
+		// Constants
 		String host = "http://localhost/chronoweb";
-		Integer baseTime = 9999;
-		Map<String, String> payload = new HashMap<>();
-		payload.put("k", "v");
 
-		for (int destination = 1; destination < neighbors + 1; destination++) {
-			Integer eventTime = baseTime + 1;
-			String postURLString = String.format("%s/graph/%s|label|%s_%s", host, source, destination, eventTime);
-			System.out.println("POST " + postURLString);
-			URL postURL = new URL(postURLString);
-			HttpURLConnection postCon = (HttpURLConnection) postURL.openConnection();
-			postCon.setRequestMethod("PUT");
-			postCon.setDoOutput(true);
-			postCon.setRequestProperty("Content-Type", "application/json");
-			postCon.setRequestProperty("Accept", "application/json");
-			OutputStreamWriter osw = new OutputStreamWriter(postCon.getOutputStream());
-			osw.write("{ \"k\": \"v\"}");
-			osw.flush();
-			osw.close();
-			postCon.getResponseCode();
-			postCon.disconnect();
-			
-			List<Integer> queue = new LinkedList<>();
-			queue.add(source);
-			List<Integer> reachableVertices = new LinkedList<>();
+		int lineRead = 0;
+		BufferedReader br = new BufferedReader(new FileReader(graphFile));
+		while (true) {
+			String line = br.readLine();
+			if (line == null)
+				break;
+
+			String[] arr = line.split("\\s");
+
+			Integer from = Integer.parseInt(arr[0]);
+			Integer to = Integer.parseInt(arr[1]);
+
+			Integer time = Integer.parseInt(arr[2]);
+
+			postEdgeEvent(host, from, to, time);
 
 			Long startTime = System.currentTimeMillis();
-			while (queue.size() > 0) {
-				Integer currVertex = queue.removeFirst();
-				String getURLString = String.format("%s/graph/%s_%s/outEe?temporalRelation=isAfter&label=label", host,
-						currVertex, baseTime - 1);
-				System.out.println("\tGET " + getURLString);
-				URL getURL = new URL(getURLString);
-
-				HttpURLConnection getCon = (HttpURLConnection) getURL.openConnection();
-				getCon.setRequestMethod("GET");
-				System.err.println(getCon.getResponseCode());
-				if (getCon.getResponseCode() == 200) {
-					BufferedReader in = new BufferedReader(new InputStreamReader(getCon.getInputStream()));
-					String inputLine;
-					List<String> edgeEvents = new LinkedList<>();
-					while ((inputLine = in.readLine()) != null) {
-						edgeEvents = Arrays.asList(inputLine.replace("[", "").replace("]", "").split(","));
-					}
-					in.close();
-
-					for (String edgeEvent : edgeEvents) {
-						System.out.println(edgeEvent);
-						try {
-							Integer inVertex = Integer.parseInt(edgeEvent.split("\\||\\||_")[2]);
-							if (!reachableVertices.contains(inVertex)) {
-								reachableVertices.add(inVertex);
-								queue.add(inVertex);
-							}
-						} catch (Exception e) {
-
-						}
-
-					}
-				}
-				getCon.disconnect();
-			}
+			getReachability(host, sourceVertex, sourceTime);
 			Long computationTime = System.currentTimeMillis() - startTime;
-			
+
 			BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, true));
-		    writer.append(computationTime.toString()).append("\n");
-		    writer.close();
+			writer.append(computationTime.toString()).append("\n");
+			writer.close();
+
+			if (++lineRead % 10000 == 0) {
+				System.out.println(lineRead + " lines read...");
+			}
 		}
+
+		br.close();
 	}
 
+	@SuppressWarnings("deprecation")
+	public static void postEdgeEvent(String host, Integer from, Integer to, Integer time) throws IOException {
+		String postURLString = String.format("%s/graph/%s|label|%s_%s", host, from, to, time);
+//		System.out.println("POST " + postURLString);
+
+		URL postURL = new URL(postURLString);
+		HttpURLConnection postCon = (HttpURLConnection) postURL.openConnection();
+		postCon.setRequestMethod("PUT");
+		postCon.setDoOutput(true);
+		postCon.setRequestProperty("Content-Type", "application/json");
+		postCon.setRequestProperty("Accept", "application/json");
+
+		OutputStreamWriter osw = new OutputStreamWriter(postCon.getOutputStream());
+		osw.write("{ \"k\": \"v\"}");
+		osw.flush();
+		osw.close();
+		postCon.getResponseCode();
+		postCon.disconnect();
+	}
+
+	@SuppressWarnings("deprecation")
+	public static void getReachability(String host, Integer sourceVertex, Integer sourceTime) throws IOException {
+		List<Integer> queue = new LinkedList<>();
+		queue.add(sourceVertex);
+		List<Integer> reachableVertices = new LinkedList<>();
+
+		while (queue.size() > 0) {
+			Integer currVertex = queue.removeFirst();
+			String getURLString = String.format("%s/graph/%s_%s/outEe?temporalRelation=isAfter&label=label", host,
+					currVertex, sourceTime);
+//			System.out.println("\tGET " + getURLString);
+			URL getURL = new URL(getURLString);
+
+			HttpURLConnection getCon = (HttpURLConnection) getURL.openConnection();
+			getCon.setRequestMethod("GET");
+//			System.err.println(getCon.getResponseCode());
+			if (getCon.getResponseCode() == 200) {
+				BufferedReader in = new BufferedReader(new InputStreamReader(getCon.getInputStream()));
+				String inputLine;
+				List<String> edgeEvents = new LinkedList<>();
+				while ((inputLine = in.readLine()) != null) {
+					edgeEvents = Arrays.asList(inputLine.replace("[", "").replace("]", "").split(","));
+				}
+				in.close();
+
+				for (String edgeEvent : edgeEvents) {
+					try {
+						Integer inVertex = Integer.parseInt(edgeEvent.split("\\||\\||_")[2]);
+						if (!reachableVertices.contains(inVertex)) {
+							reachableVertices.add(inVertex);
+							queue.add(inVertex);
+						}
+					} catch (Exception e) {
+					}
+				}
+			}
+			getCon.disconnect();
+		}
+	}
 }
