f6e75841c98b6743f245dacb754cb3113dbbc021
Jaewook Byun <bjw0829@gmail.com>
2024-05-17 17:48:00 +0900

    gamma table search ui (ing)

--- chronoweb/src/main/resources/webroot/js/chronoweb.js
+++ chronoweb/src/main/resources/webroot/js/chronoweb.js
@@ -14,10 +14,74 @@ function serverConn(url, circle) {
 		url: url,
 		crossOrigin: true,
 	}).done(() => {
-		$(circle).removeClass('bg-danger').addClass("bg-success");	
+		$(circle).removeClass('bg-danger').addClass("bg-success");
 	}).fail(() => {
-		
+
 		console.log("abc");
 		$(circle).removeClass('bg-success').addClass("bg-danger");
+	});
+}
+
+function getGammaTableSources(url, sourceContainerID, sourceMenuID){
+	$.ajax({
+		type: "GET",
+		url: url,
+		crossOrigin: true,
+	}).done((result) => {
+		console.log(result);
+		$("#" + sourceContainerID).html("");
+		for (let key in result) {
+			// console.log(result[key]);
+			var newItem = $('<a class="dropdown-item" href="#">' + result[key] + '</a>');
+			newItem.click(function() {	
+				$("#" + sourceMenuID).text($(this).text());
+				// getGammaTablePrograms(url, programContainerID, programMenuID);
+			});	
+			$("#" + sourceContainerID).append(newItem);
+		}
+	}).fail((result) => {
+		console.log(result);
+	});
+}
+
+function getGammaTablePrograms(url, programContainerID, programMenuID, sourceContainerID, sourceMenuID){
+	$.ajax({
+		type: "GET",
+		url: url,
+		crossOrigin: true,
+	}).done((result) => {
+		
+		$("#" + programContainerID).html("");
+		for (var key in result) {
+			
+			var newItem = $('<a class="dropdown-item" href="#">' + result[key] + '</a>');
+			newItem.click(function() {	
+				$("#" + programMenuID).text($(this).text());
+				getGammaTableSources(url + "/" + result[key] + "/link", sourceContainerID, sourceMenuID);
+			});	
+			$("#" + programContainerID).append(newItem);
+		}
+	}).fail((result) => {
+		console.log(result);
+	});
+}
+
+function getGammaTableStartTimes(url, timeContainerID, timeMenuID, programContainerID, programMenuID, sourceContainerID, sourceMenuID) {
+	$.ajax({
+		type: "GET",
+		url: url,
+		crossOrigin: true,
+	}).done((result) => {
+		$("#" + timeContainerID).html("");
+		for (key in result) {
+			var newItem = $('<a class="dropdown-item" href="#">' + result[key] + '</a>');
+			newItem.click(function() {	
+				$("#" + timeMenuID).text($(this).text());
+				getGammaTablePrograms(url+"/"+result[key], programContainerID, programMenuID, sourceContainerID, sourceMenuID);
+			});	
+			$("#" + timeContainerID).append(newItem);
+		}
+	}).fail((result) => {
+		console.log(result);
 	});
 }
